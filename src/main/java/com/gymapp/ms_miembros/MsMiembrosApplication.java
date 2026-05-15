4b8fb3b434a7eb8a2739c1c316046e58101fec7a
cconstanz <co.cerdab@duocuc.cl>
2026-05-15 13:22:30 -0400

    Actualizacion en las interfaces y properties en ms-miembros

--- src/main/java/com/gymapp/ms_miembros/MsMiembrosApplication.java
+++ src/main/java/com/gymapp/ms_miembros/MsMiembrosApplication.java
@@ -2,8 +2,10 @@ package com.gymapp.ms_miembros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsMiembrosApplication {
 
 	public static void main(String[] args) {
