c51e486d6d9129541081d4b458ab724aee081e71
cconstanz <co.cerdab@duocuc.cl>
2026-06-21 15:16:57 -0400

    security: implementacion de capa de seguridad y filtros JWT
    - Configuración de SecurityFilterChain.
    
    - Implementación de JwtService para generación/validación de tokens.
    
    - Integración de JwtAuthenticationFilter en la cadena de peticiones.

--- src/main/java/com/gymapp/ms_miembros/config/SecurityConfig.java
+++ src/main/java/com/gymapp/ms_miembros/config/SecurityConfig.java
@@ -6,25 +6,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
+
+// @Configuration
+// @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/api/miembros/**");
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // Se desactiva porque usamos tokens JWT
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        // Aquí podrías agregar rutas públicas si las tuvieras, por ejemplo:
-                        // .requestMatchers("/api/miembros/publico/**").permitAll()
-                        .anyRequest().authenticated() // Obliga a que cualquier otra petición tenga un JWT válido
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
